c6d753b95f365f8616dadf653eaf75b6035df348
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-04-02 18:19:57 +0500

    tachskils

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -4,6 +4,7 @@ import helloimg from '../../assets/img/helloimg.png';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import './home.css';
+import Skils from '../techskils/techscils.component';
 
 const Home = () => {
   return (
--- src/components/techskils/techscils.component.jsx
+++ src/components/techskils/techscils.component.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Skils = () => {
   return (
-    <div>Skils</div>
-  )
-}
+    <div className='flex items-center w-[630px] absolute left-20 bottom-0'>
+      <div>Tech Stack |</div>
+      <div className='flex'>
+        <h1>1</h1>
+        <h1>1</h1>
+        <h1>1</h1>
+        <h1>1</h1>
+        <h1>1</h1>
+        <h1>1</h1>
+        <h1>1</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Skils
+export default Skils;
