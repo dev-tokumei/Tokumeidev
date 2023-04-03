47f975684dd4436031bc61be7209b1e28962b101
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-04-03 12:55:15 +0500

    chage color border logo

--- src/common/navbar/navbar.component.jsx
+++ src/common/navbar/navbar.component.jsx
@@ -10,11 +10,13 @@ const Navbar = () => {
       <nav className='w-full flex items-center px-10 h-[90px] py-[20px] justify-between backdrop-blur-sm bg-white/30 fixed z-[100] shadow-md'>
         <a href='#home'>
           <div className='font-bold text-black text-2xl cursor-pointer flex items-center justify-between font-[Poppins]'>
-            <img
-              className='mobile w-[60px] h-[60px] rounded-full img_shadow'
-              src={logo}
-              alt='logo'
-            />
+            <div className='w-[67px] h-[67px] img_shadow'>
+              <img
+                className='mobile w-[60px] h-[60px] rounded-full logo '
+                src={logo}
+                alt='logo'
+              />
+            </div>
             <span className='sm:text-2xl md:text-3xl mr-1 ml-4 '>Tokumei</span>
           </div>
         </a>
