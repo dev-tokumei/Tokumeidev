59b13f79ad40723ecf6d80b8a3da6a15c0dcb070
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-04-03 15:14:42 +0500

    about img

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,10 +1,27 @@
-import { Container } from '@mui/material';
 import React from 'react';
+import { Container } from '@mui/material';
+import note from '../../assets/img/nout.webp';
 
 const About = () => {
   return (
     <Container>
-      <div id='about' className='w-full h-screen snap-center'>About</div>
+      <div
+        id='about'
+        className='w-full flex flex-col h-screen snap-center justify-center items-center'
+      >
+        <div className='flex justify-between items-center'>
+          <div className='w-[410px] h-[350px]'>
+            <img
+              className='w-full h-full  rounded-3xl'
+              src={note}
+              alt='notebook'
+            />
+          </div>
+          <div>
+            <div>ABOUT ME</div>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 };
