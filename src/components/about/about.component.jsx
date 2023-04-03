0b608cf85f958178f19b3e44b861ef01f6bda088
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-04-03 16:32:25 +0500

    spiner text

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import note from '../../assets/img/nout.webp';
+import mac from '../../assets/img/mac.png';
+import './about.css'
 
 const About = () => {
   return (
@@ -10,12 +12,15 @@ const About = () => {
         className='w-full flex flex-col h-screen snap-center justify-center items-center'
       >
         <div className='w-full flex justify-evenly items-center'>
-          <div className='w-[410px] h-[350px]'>
+          <div className='w-[410px] h-[350px] relative'>
             <img
               className='w-full h-full  rounded-3xl'
               src={note}
               alt='notebook'
             />
+            <div className='flex justify-center loader items-center w-[180px] h-[180px] bg-red-500 rounded-full bottom-[-50px] right-[-47px] absolute'>
+              <img className='w-[60px] h-[60px]' src={mac} alt='mac' />
+            </div>
           </div>
           <div className='w-[480px]'>
             <div>
@@ -27,12 +32,12 @@ const About = () => {
             <p className='text-[#767676] font-medium text-[17px]'>
               Являясь мидл фронтенд-разработчиком, я обладаю внушительным
               арсеналом навыков в HTML, CSS, Tailwind, JavaScript, React, Redux,
-              и Typescript. Я преуспеваю в разработке и поддержке адаптивных веб-сайтов,
-              которые обеспечивают удобство работы пользователей. Мой опыт
-              заключается в создании динамичных, привлекательных интерфейсов
-              путем написания чистого и оптимизированного кода и использования
-              передовых инструментов и методов разработки. Я также являюсь
-              командным игроком, который преуспевает в сотрудничестве с
+              и Typescript. Я преуспеваю в разработке и поддержке адаптивных
+              веб-сайтов, которые обеспечивают удобство работы пользователей.
+              Мой опыт заключается в создании динамичных, привлекательных
+              интерфейсов путем написания чистого и оптимизированного кода и
+              использования передовых инструментов и методов разработки. Я также
+              являюсь командным игроком, который преуспевает в сотрудничестве с
               кросс-функциональными командами для создания выдающихся
               веб-приложений.
             </p>
