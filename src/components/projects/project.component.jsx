7e5732131e17597c843ff98ef987b3a98fa16609
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-04-04 17:38:43 +0500

    edit stact title in project object

--- src/components/projects/project.component.jsx
+++ src/components/projects/project.component.jsx
@@ -17,7 +17,7 @@ const projects = [
     name: 'Gravity.Studio',
     title:
       'A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars',
-    stack: { vue: 'Vue JS', tw: 'Tailwind css' },
+    stack: { vue: 'React', tw: 'Tailwind css' },
     src: gravity,
     link: 'https://gravity-ruby.vercel.app/',
   },
