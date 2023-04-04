06baf63e36116bf06d768e5d21e824cec130035c
Subhoniddin <subhoniddinodinaev@gmail.com>
2023-04-04 13:33:51 +0500

    add pt in about_text_block

--- src/components/projects/project.component.jsx
+++ src/components/projects/project.component.jsx
@@ -28,7 +28,7 @@ const Project = () => {
     <div className='w-full bg-[#f9f9f9] pt-20'>
       <Container>
         <div className='w-full justify-center snap-center py-20'>
-          <div>
+          <div className='block_text_project'>
             <h1 className='text-[#147efb] font-bold text-lg mb-4'>PORTFOLIO</h1>
             <p className='text-[#2d2e32] font-bold text-3xl'>
               Каждый проект — это уникальная разработка 🧩
